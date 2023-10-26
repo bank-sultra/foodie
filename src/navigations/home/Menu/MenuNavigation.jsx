6f098b532cf404bbfd1edce80d479f08f0f75a93
Rizaldy Setiawan Hasanuddin <rsh.newarray@gmail.com>
2023-10-26 11:26:48 +0800

    fix - nav name

---
+++ src/navigations/home/Menu/MenuNavigation.jsx
@@ -0,0 +1,17 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react'; 
+import Cart from '../../../screens/home/menu/cart';
+import Maps from '../../../screens/maps';
+
+const Stack = createStackNavigator();
+
+const MenuNavigation = () => {
+    return (
+        <Stack.Navigator initialRouteName="Cart">
+            <Stack.Screen name="Cart" component={Cart} />
+            <Stack.Screen name="Maps" component={Maps} />
+        </Stack.Navigator>
+    )
+}
+
+export default MenuNavigation
