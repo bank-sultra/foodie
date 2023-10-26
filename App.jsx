0cebd30db31cea010e27ca2e6acb8cd475ae2a0b
codesprint.id <codesprint.id@gmail.com>
2023-10-26 10:42:16 +0800

    Navigations

--- App.jsx
+++ App.jsx
@@ -1,18 +1,10 @@
+import { StyleSheet } from 'react-native';
 import 'react-native-gesture-handler';
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-
-const Stack = createStackNavigator();
+import Root from './src/navigations/RootNavigation';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Root />
   );
 }
 
