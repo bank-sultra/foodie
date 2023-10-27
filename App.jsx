cd367b06b1b4fcf9f036f480dcc418f9197c4b92
Rizaldy Setiawan Hasanuddin <rsh.newarray@gmail.com>
2023-10-27 10:37:24 +0800

    home: connect firestore

--- App.jsx
+++ App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import Root from './src/navigations/RootNavigation'
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <Root />
+    <QueryClientProvider client={queryClient}>
+      <Root />
+    </QueryClientProvider>
   )
 }
